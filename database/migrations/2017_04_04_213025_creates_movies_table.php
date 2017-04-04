994f37d9cf4a58cb1b6e6ffb3d1cb7d403ea0df1
Alma Washington <alma19@live.unc.edu>
2017-04-04 17:48:17 -0400

    Initial commit.

---
+++ database/migrations/2017_04_04_213025_creates_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatesMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('movies', function(Blueprint $table) {
+        $table->increments('id');
+        $table->string('title');
+        $table->string('director');
+        $table->string('genre');
+        $table->string('status');
+        $table->string('notes');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
